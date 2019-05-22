1c7745750c03abfd66ccfb94ac268112741d5a1f
Jenny <jennyelisa07@aol.com>
2019-05-21 23:21:08 -0400

    game completed for the most part

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,11 +34,12 @@ $(document).ready(function() {
     $('#start-screen').show();
     $('#game').hide();
     $('#stats-screen').hide();
-    
+    var resetAnswer= false;
+    var rightAnswersDom = document.getElementById("correct");
+    var wrongAnswerDom = document.getElementById("incorrect");
     var rightAnswers = 0; 
     var wrongAnswers = 0;
-    var noAnswers = 0;
-
+    var timeClock = document.getElementById("timer");
     var timeRemaining = 60;
     var intervalTime;
     var clockRunning = false;
@@ -84,8 +85,7 @@ $(document).ready(function() {
                 // countdown from 45 sec
                 timeRemaining--;
                 console.log(timeRemaining)
-                // var timeClock = $("<p>");
-                // timeClock.html(timeRemaining).appendTo("#timer");
+                 timeClock.innerHTML="Time Remaining: " + timeRemaining;
                 // this works but each second is shown!!!!!
                 // when time is 0 will go to stats page, gameover
                 if(timeRemaining === 0) {
@@ -96,141 +96,147 @@ $(document).ready(function() {
             }, 
             1000);
             clockRunning = true;
-           
-            // Math.floor((t % (1000 * 60)) / 1000); calculates seconds??
+            
             creatingAnswers();
 
         }
     }
 
  function creatingAnswers () {
+     if (resetAnswer === false){
         for (var i = 0; i < quotes[0].answers.length; i++) {
-          var radioBtn = ($('<label for=""> <input type="radio" id="" name="pickedAnswer0"> '));
-          $(radioBtn).attr("id", quotes[0].answers[i]);
-          var span = $("<span>");
-          $(span).attr("id", quotes[0].answers[i]);
-          $(span).text(quotes[0].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question0");
 
-          radioBtn = ($('<label for=""><input type="radio" id="answer" name="pickedAnswer1"> '));
-          $(radioBtn).attr("id", quotes[1].answers[i]);
-          var span1 = $("<span>");
-          $(span1).attr("id", quotes[1].answers[i]);
-          $(span1).text(quotes[1].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question1");
+            var radioBtn = ($('<label for="" >'));
+            radioBtn.attr('name', "answer");
+            var inputBtn = $("<input type='radio' class='pickedAnswer0' name='answer0'>");
+            inputBtn.attr("id", quotes[0].answers[i]);
+            var span0 = $("<span>");
+            $(span0).attr("id", quotes[0].answers[i]);
+            $(span0).text(quotes[0].answers[i]).appendTo(radioBtn);
+            $(inputBtn).appendTo(radioBtn);
+            $(radioBtn).appendTo(".question0");
 
-          radioBtn = ($('<label for=""><input type="radio" id="answer" name="pickedAnswer2"> '));
-          $(radioBtn).attr("id", quotes[2].answers[i]);
-          var span2 = $("<span>");
-          $(span2).attr("id", quotes[2].answers[i]);
-          $(span2).text(quotes[2].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question2");
 
-          radioBtn = ($('<label for=""><input type="radio" id="answer" name="pickedAnswer3"> '));
-          $(radioBtn).attr("id", quotes[3].answers[i]);
-          var span3 = $("<span>");
-          $(span3).attr("id", quotes[3].answers[i]);
-          $(span3).text(quotes[3].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question3");
+           radioBtn = ($('<label for="">'));
+           inputBtn = $("<input type='radio' class='pickedAnswer1' name='answer1'>");
+           inputBtn.attr("id", quotes[1].answers[i]);
+           var span1 = $("<span>");
+           $(span1).attr("id", quotes[1].answers[i]);
+           $(span1).text(quotes[1].answers[i]).appendTo(radioBtn);
+           $(inputBtn).appendTo(radioBtn);
+           $(radioBtn).appendTo(".question1");
 
-          radioBtn = ($('<label for=""><input type="radio" id="answer" name="pickedAnswer4"> '));
-          $(radioBtn).attr("id", quotes[4].answers[i]);
-          var span4 = $("<span>");
-          $(span4).attr("id", quotes[4].answers[i]);
-          $(span4).text(quotes[4].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question4");
+           radioBtn = ($('<label for="">'));
+           inputBtn = $("<input type='radio' class='pickedAnswer2' name='answer2'>");
+           inputBtn.attr("id", quotes[2].answers[i]);
+           var span2 = $("<span>");
+           $(span2).attr("id", quotes[2].answers[i]);
+           $(span2).text(quotes[2].answers[i]).appendTo(radioBtn);
+           $(inputBtn).appendTo(radioBtn);
+           $(radioBtn).appendTo(".question2");
 
-          radioBtn = ($('<label for=""><input type="radio" id="answer" name="pickedAnswer5"> '));
-          $(radioBtn).attr("id", quotes[5].answers[i]);
-          var span5 = $("<span>");
-          $(span5).attr("id", quotes[5].answers[i]);
-          $(span5).text(quotes[5].answers[i]).appendTo(radioBtn);
-          $(radioBtn).appendTo(".question5");
+           radioBtn = ($('<label for="">'));
+           inputBtn = $("<input type='radio' class='pickedAnswer3' name='answer3'>");
+           inputBtn.attr("id", quotes[3].answers[i]);
+           var span3 = $("<span>");
+           $(span3).attr("id", quotes[3].answers[i]);
+           $(span3).text(quotes[3].answers[i]).appendTo(radioBtn);
+           $(inputBtn).appendTo(radioBtn);
+           $(radioBtn).appendTo(".question3");
+
+           radioBtn = ($('<label for="">'));
+           inputBtn = $("<input type='radio' class='pickedAnswer4' name='answer4'>");
+           inputBtn.attr("id", quotes[4].answers[i]);
+           var span4 = $("<span>");
+           $(span4).attr("id", quotes[4].answers[i]);
+           $(span4).text(quotes[4].answers[i]).appendTo(radioBtn);
+           $(inputBtn).appendTo(radioBtn);
+           $(radioBtn).appendTo(".question4");
+
+           radioBtn = ($('<label for="">'));
+           inputBtn = $("<input type='radio' class='pickedAnswer5' name='answer5'>");
+           inputBtn.attr("id", quotes[5].answers[i]);
+           var span5 = $("<span>");
+           $(span5).attr("id", quotes[5].answers[i]);
+           $(span5).text(quotes[5].answers[i]).appendTo(radioBtn);
+           $(inputBtn).appendTo(radioBtn);
+           $(radioBtn).appendTo(".question5");
         }
- }
-// when game reset button clicked, stop answers from appearing again???
-
-
-
-function checkingAnswers () {
-    if ($("input[type='radio'][class='question0'][id ='answer']:checked").val()) {
-        if ($(radioBtn).attr("checked", "checked")) {
-        console.log('p is right');
-        rightAnswers++;
     }
-    else {
-      console.log('Wrong');
-        wrongAnswers++;
-    }
-
-// for(var i = 1; i <= ; i++) {
-//   var radios = document.getElementsByName("pickedAnswer0" + i);
-//   for(var j = 0; j < radios.length; j++) {
-//     var radio = radioBtn[j];
-//     if(radioBtn.value === "correct" && radio.checked) {
-//       rightAnswers++;
-//     }
-//   }
+    resetAnswer = true;
  }
-}
+
 
 
 $('#timer-done').click(function() {
-    // if (!$("input[type='radio'][class='question0'][id ='answer']:checked").val()) {
-    //    alert('Nothing is checked!');
-    //     return false;
-    if (!$("input[id='pocahantas']:checked").val()) {
-        console.log('!');
-         return false; 
-    } if (!$("input[id='lilo $ stitch']:checked").val()) {
-        console.log('Nothing is 1!');
-         return false; 
-    } if (!$("input[name='pickedAnswer2']:checked").val()) {
-        console.log('Nothing is checked!2');
-         return false;
-    } if (!$("input[name='pickedAnswer3']:checked").val()) {
-        console.log('Nothing is checked3!');
-         return false;
-    }if (!$("input[name='pickedAnswer4']:checked").val()) {
-        console.log('Nothing is checked4!');
-         return false;
-    }if (!$("input[name='pickedAnswer5']:checked").val()) {
-        console.log('Nothing is checked5!');
-         return false;
-    }
-    else {
-      alert("something clicked");
-      wrongAnswers++;
-    //   $(wrongAnswers).text("#incorrect");
-    }
-    rightAnswers++;
-  });
-      //this does check if every question has an answer otherwise it says nothing is checked
- 
+    rightAnswers = 0;
+    wrongAnwers = 0;
 
+    if ($("input[type='radio'][class='pickedAnswer0'][id='Pocahontas']:checked").val()) {
+        console.log('Pocahontas');
+        rightAnswers++;
+    } else {
+        wrongAnswers++;
+        //   $(wrongAnswers).text("#incorrect");
+    }
+    if ($("input[type='radio'][class='pickedAnswer1'][id='Lilo & Stitch']:checked").val()) {
+        console.log('Lilo and Stitch');
+        rightAnswers++;
+    } else {
+        wrongAnswers++;
+
+    }
+    if ($("input[type='radio'][class='pickedAnswer2'][id='Mulan']:checked").val()) {
+        console.log('Mulan');
+        rightAnswers++;
+
+    } else {
+        wrongAnswers++;
+    }
+    if ($("input[type='radio'][class='pickedAnswer3'][id='The AristoCats']:checked").val()) {
+        console.log('The AristoCats');
+        rightAnswers++;
+
+    } else {
+        wrongAnswers++;
+
+    }
+    if ($("input[type='radio'][class='pickedAnswer4'][id='Alice In Wonderland']:checked").val()) {
+        console.log('Alice in Wonderland');
+        rightAnswers++;
+
+    } else {
+        wrongAnswers++;
+
+    }
+    if ($("input[type='radio'][class='pickedAnswer5'][id='Peter Pan']:checked").val()) {
+        console.log('Peter Pan');
+        rightAnswers++;
+    } else {
+        wrongAnswers++;
+    }
+    console.log(wrongAnswers);
+    console.log(rightAnswers);
+     doneGame();
+  }); 
 
 
     function doneGame() {
-        console.log('YA DONE');
+        console.log('GAMEOVER!');
         clearInterval(intervalTime);
         clockRunning = false;
         $('#game').hide();
         $('#stats-screen').show();
         //this works if i change the number of right/wrong answers
         if (rightAnswers <= 3) {
-            alert("Nice Try!");
+            //  alert("Nice Try!");
         } else if (rightAnswers >= 4) {
-            alert("Good Job!")
+            //  alert("Good Job!")
         };
- 
-
+        rightAnswersDom.innerHTML = "Correct: " + rightAnswers;
+        wrongAnswerDom.innerHTML = "Incorrect: " + wrongAnswers;
     }
-    var rightAnswerDom = $("<p>");
-    // $(rightAnswerDom).attr("id", correct);
-    rightAnswerDom.text(rightAnswers).appendTo("#correct");
-
-    var wrongAnswerDom = $("<p>");
-    wrongAnswerDom.text(wrongAnswers).appendTo("#incorrect");
+    
 
     // need reset game function to link reset game button
 
@@ -238,13 +244,6 @@ $('#timer-done').click(function() {
 function reset() {
     $("#start-screen").show();
     $("#stats-screen").hide();
-    // creatingAnswers();
 }
-
-
-// document.getElementById('rightAnswer').innerHTML = "Correct answers " + rightAnswers;
-// document.getElementById('wrongAnswer').textContent = "Incorrect answers " + wrongAnswers;
-// document.getElementById('noAnswer').textContent = "Unanswered " + noAnswers;
-
 
 });
